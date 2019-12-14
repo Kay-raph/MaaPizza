3c2e9d53fc3cec10b1a8a0b4ea81a03ea232b7a9
kevin raphael mugita <52208503+Kay-raph@users.noreply.github.com>
2019-12-15 00:07:59 +0300

    Add files via upload

---
+++ order/js/script2.js
@@ -0,0 +1,8 @@
+var Pizza="pizza";
+var pizza={
+pizza.size:["large", "medium", "small"],
+pizza.crust:["crispy", "stuffed", "gluten-free"],
+large.price:["crispy=ksh800", "stuffed=ksh700", "gluten-free=ksh850"],
+medium.price:["crispy=ksh600", "stuffed=ksh500", "gluten-free=ksh650"],
+small.price:["crispy=ksh300", "stuffed=ksh200", "gluten-free=ksh350"]
+};
