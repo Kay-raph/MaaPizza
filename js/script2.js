4eb46863e30aee88fdb7180331232cffbd7e7ff7
kevin raphael mugita <52208503+Kay-raph@users.noreply.github.com>
2019-12-15 00:06:45 +0300

    Delete script2.js

--- js/script2.js
+++
@@ -1,8 +0,0 @@
-var Pizza="pizza"
-var pizza={
-pizza.size:["large", "medium", "small"],
-pizza.crust:["crispy", "stuffed", "gluten-free"],
-large.price:["crispy=ksh800", "stuffed=ksh700", "gluten-free=ksh850"],
-medium.price:["crispy=ksh600", "stuffed=ksh500", "gluten-free=ksh650"],
-small.price:["crispy=ksh300", "stuffed=ksh200", "gluten-free=ksh350"]
-};
